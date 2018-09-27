5e6fc9b2cad15870e0c130bf898b2fdae8048390
lee0c <lee.cattarin@gmail.com>
2018-09-27 15:11:03 -0700

    Updated code to EH 1.2.0

--- src/main/java/ProxyEventHubTest.java
+++ src/main/java/ProxyEventHubTest.java
@@ -2,20 +2,35 @@ import com.microsoft.azure.eventhubs.*;
 import com.microsoft.azure.eventhubs.EventHubClient;
 
 import java.io.IOException;
+import java.net.*;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ProxyEventHubTest
 {
-    private static final String PROXY_HOSTNAME_NAME = "PROXY_HOSTNAME";
-    private static final String PROXY_PORT_NAME = "PROXY_PORT";
-    private static final String EVENT_HUB_CONNECTION_STRING_NAME = "EVENT_HUB_CONNECTION_STRING";
+    private static final String EVENT_HUB_CONNECTION_STRING_ENV_VAR = "EVENT_HUB_CONNECTION_STRING";
+    private static final String PROXY_HOSTNAME_ENV_VAR = "PROXY_HOSTNAME";
+    private static final String PROXY_PORT_ENV_VAR = "PROXY_PORT";
 
-    private static String CONNECTION_STRING = System.getenv(EVENT_HUB_CONNECTION_STRING_NAME);
+    private static String CONNECTION_STRING = System.getenv(EVENT_HUB_CONNECTION_STRING_ENV_VAR);
+    private static String PROXY_HOSTNAME = System.getenv(PROXY_HOSTNAME_ENV_VAR);
+    private static int PROXY_PORT = Integer.valueOf(System.getenv(PROXY_PORT_ENV_VAR));
 
     public static void main (String[] args) throws EventHubException, IOException {
-        EventHubClient.setProxyHostName(System.getenv(PROXY_HOSTNAME_NAME));
-        EventHubClient.setProxyHostPort(Integer.parseInt(System.getenv(PROXY_PORT_NAME)));
+        ProxySelector.setDefault(new ProxySelector() {
+            @Override
+            public List<Proxy> select(URI uri) {
+                LinkedList<Proxy> proxies = new LinkedList<>();
+                proxies.add(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(PROXY_HOSTNAME, PROXY_PORT)));
+                return proxies;
+            }
+            @Override
+            public void connectFailed(URI uri, SocketAddress sa, IOException ioe) {
+                // no-op
+            }
+        });
 
         final ConnectionStringBuilder connStr = new ConnectionStringBuilder(CONNECTION_STRING);
         connStr.setTransportType(TransportType.AMQP_WEB_SOCKETS);
